89e5082686ee8f1ef565ae2a05f09dbd9bd43167
Yang-Seung-Ho <ju5979@stu.jejunu.ac.kr>
2025-03-17 14:12:04 +0900

    투자계산기파일생성

---
+++ udemy-project/01-starting-project/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import logo from "../assets/investment-calculator.png"
+
+export default function Header() {
+    return <header>
+        <img src={logo} alt="logo" />
+        <h1>Investment Calculator</h1>
+    </header>
+}
